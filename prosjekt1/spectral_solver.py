b23b0b1306af094a5c8d72789ef55b878fc2b245
Mikal Stapnes <mikalst@gmail.com>
2017-02-11 21:31:51 +0100

    Finished prosjekt1

--- prosjekt1/spectral_solver.py
+++ prosjekt1/spectral_solver.py
@@ -1,78 +1,93 @@
-import math
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
 
-def spectral_laplace_lhs(x_points):
-    """
-    Takes in an array of x grid points and generates a fitting diffmatrix
-    """
-    x_length = len(x_points)
-    matrix_a = np.zeros((x_length, x_length))
-    for i in range(x_length-1):
-        matrix_a[i, i] = -2
-        matrix_a[i+1, i] = 1
-        matrix_a[i, i+1] = 1
-    matrix_a[x_length-1, x_length-1] = -2
-    print np.matrix(matrix_a)
-    return matrix_a
+def lagrange (x_values, indeks, x):
+    teller = 1
+    nevner = 1
+    for i in range (len(x_values)):
+        if i != indeks:
+            teller = teller*(x - x_values[i])
+            nevner = nevner*(x_values[indeks] - x_values[i])
+    return teller / nevner
 
-def spectral_laplace_rhs(x_points, math_function, start, end):
-    """
-    Returns the right hand side of the eqation AU = B which needs
-    to be solved in a Dirichlet boundary problem.
-    Params:
-     x - array of x grid points
-     f - mathematical function
-     ua - initial boundary value
-     ub - concluding boundary value
-   """
-
-    x_length = len(x_points)
-    start_x = x_points[0]
-    end_x = x_points[x_length-1]
-    diff_x = (end_x-start_x)/x_length
-    matrix_b = np.zeros(x_length)
-    for i in range(1, x_length-2):
-        matrix_b[i] = math_function(x_points[i])*diff_x**2
-    matrix_b[0] = math_function(x_points[0])*diff_x**2-start
-    matrix_b[-1] = math_function(x_points[-1])*diff_x**2-end
-    print matrix_b
-    return matrix_b
-
-def spectral_laplace(x_points, math_function, start_value, end_value):
-    """
-    Fetches both LHS and RHS of the equation UA = B and solves for matrix U
-    """
-    matrix_a = spectral_laplace_lhs(x_points)
-    matrix_b = spectral_laplace_rhs(x_points, math_function, start_value, end_value)
-    return np.linalg.solve(matrix_a, matrix_b)
-
-def calculate_laplace_polynomial(x_uniform, math_f, start_value, end_value):
-    """
-    NOT YET FINISHED
-    """
-    u_matrix = spectral_laplace(x_uniform, math_f, start_value, end_value)
+def dd_lagrange (x_values, indeks, x):
+    teller = 0
+    nevner = 1
+    for i in range(len(x_values)):
+        if i != indeks:
+            nevner *= (x_values[indeks] - x_values[i])
+    for i in range(len(x_values)):
+        if i != indeks:
+            for j in range(len(x_values)):
+                if j != indeks and j != i:
+                    ledd = 1
+                    for l in range(len(x_values)):
+                        if l != indeks and l != i and l != j:
+                            ledd *= (x - x_values[l])
+                    teller += ledd
+    return teller / nevner
     
-
-
+def spectral_laplace(x_values, dd_math_function, sigma, ua, ub):
+    """Takes in interpolation points x_points, double derivative and start end points to
+    numerically solve the Poisson-problem with the two Dirichlet-values
+    """
+    B = []
+    for x in x_values:
+        B += [-dd_math_function(x, sigma)]
+    B[0] = ua
+    B[len(x_values) - 1] = ub
+    #B ferdig
+    A=[]
+    for i in range (len(x_values)):
+        a = []
+        for j in range (len(x_values)):
+            if i == 0 or i == len(x_values) - 1:
+                a.append(lagrange(x_values, j, x_values[i]))
+            else:
+                a.append(dd_lagrange(x_values, j, x_values[i]))
+        A.append(a)
+    #A ferdig
+    return np.linalg.solve(A, B)
+    
 def main():
-    """
-    main
-    """
 
-    def math_f(x_value):
-        """
-        math_f
-        """
-        return math.exp(x_value)*math.cos(8*math.pi*x_value)
-
-    start_value = 0
-    end_value = 1
-    x_uniform = np.linspace(start_value, end_value, 10)
-    print u_matrix
-    plt.plot(x_uniform, u_matrix)
-    plt.show()
+    ua = -1
+    ub = 1
+    x_start = 0
+    x_end = 10
+    u_avg = 3.0
+    N = 35
+    upper = 2000.
+    lower = 0.
+    
+    def f(x, sigma):
+        mu = 2.0
+        return np.exp(-(x-mu)**2/(sigma**2))
+    
+    x_values = (x_end + x_start)/2 - (x_end - x_start)/2*np.cos(np.arange(N)*np.pi/(N-1))
+    print(x_values)
+    
+    diff_u = 1
+        
+    while np.abs(diff_u) > 0.000001:
+        sigma = (lower + upper)/2
+        print(sigma)
+        u_values = spectral_laplace(x_values, f, sigma, ua, ub)
+        
+        approx_integral = 0
+        for j in range(len(u_values)-1):
+            approx_integral += (u_values[j] + u_values[j+1])/2*(x_values[j+1]-x_values[j])
+        
+        diff_u = u_avg - approx_integral/(x_end - x_start)
+        print(diff_u)
+        if diff_u > 0:
+            lower = sigma
+        else:
+            upper = sigma
+            
+        plt.axis([0, 10, -1, 5])
+        plt.plot(x_values, u_values)
+        plt.show(block=True)
 
 if __name__ == "__main__":
     main()
